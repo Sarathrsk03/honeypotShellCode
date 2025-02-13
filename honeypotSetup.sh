c0ce88d11b5b84079ff0a25a4c4d71ed8f176835
Sarath Rajan Senthilkumar <118745490+Sarathrsk03@users.noreply.github.com>
2025-02-13 17:31:31 +0530

    Update honeypotSetup.sh

--- honeypotSetup.sh
+++ honeypotSetup.sh
@@ -1,1 +1,28 @@
+# !/bin/bash 
+
+sudo apt-get install -y python3-all 
+sudo apt-get install -y git 
+sudo apt install -y python3.10-venv
+
+git clone https://github.com/Sarathrsk03/testHoneyPot
+
+cd testHoneyPot 
+
+python3 -m venv venv 
+
+source venv/bin/activate
+
+pip install -r requirements.txt
+
+
+read -p "Enter the company name: " companyNameInp 
+read -p "Enter the industry: " industryInp
+read -p "Enter the role: " roleInp
+export companyName=$companyNameInp
+export industry=$industryInp
+export role=$roleInp
+
+export geminiAPI=
+
+python3 generatePrompts.py
 
